9e3ae068ca81405e3efc1d5770c708d5743b6dbe
CHRONO\chrono <chrono059@gmail.com>
2019-10-20 18:08:15 +0300

    Добавлена десериализация результата запроса в List

--- src/main/java/basic/DBClient.java
+++ src/main/java/basic/DBClient.java
@@ -1,14 +1,64 @@
 package basic;
 
 import java.sql.*;
+import java.util.*;
 
 /**
- * DBClient
+ * DBClient - Выполняет простейший заброс к базе данных на MySQL
  */
 public class DBClient {
 
-    public static void main(String[] args) throws SQLException {
-        printRawDataFromMySQL();
+    public static void main(String[] args) {
+        try {
+            printRawDataFromMySQLAdvanced();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printRawDataFromMySQLAdvanced() throws SQLException {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/world?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+                    "root", "root");
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM country ORDER BY Population DESC LIMIT 10");
+            List<Map<String, Object>> result = resultSetToList(rs);
+            stmt.close();
+
+            for (Map<String, Object> item : result) {
+                System.out.println(String.format("%-20s%20d", item.get("Name"), item.get("Population")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    /**
+     * Convert the ResultSet to a List of Maps, where each Map represents a row with
+     * columnNames and columValues
+     * 
+     * @param rs
+     * @return
+     * @throws SQLException
+     */
+    private static List<Map<String, Object>> resultSetToList(ResultSet rs) throws SQLException {
+        ResultSetMetaData md = rs.getMetaData();
+        int columns = md.getColumnCount();
+        List<Map<String, Object>> rows = new ArrayList<Map<String, Object>>();
+        while (rs.next()){
+            Map<String, Object> row = new HashMap<String, Object>(columns);
+            for(int i = 1; i <= columns; ++i){
+                row.put(md.getColumnName(i), rs.getObject(i));
+            }
+            rows.add(row);
+        }
+        return rows;
     }
 
     /**
