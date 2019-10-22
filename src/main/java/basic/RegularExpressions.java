e4395d5cdbbf20a4b94bd6624e67fc857eeb16d4
CHRONO\chrono <chrono059@gmail.com>
2019-10-23 01:01:51 +0300

    Добавлено несколько паттернов

--- src/main/java/basic/RegularExpressions.java
+++ src/main/java/basic/RegularExpressions.java
@@ -9,8 +9,12 @@ import java.util.regex.Pattern;
 public class RegularExpressions {
 
     public static void main(String[] args) {
-        String multilineText = "на самом деле регулярное выражение\n" + "это шаблон для поиска строки в\n" + "тексте \n"
-                + "В Java исходным представлением этого\n" + " шаблона";
+        String multilineText =  "на самом деле регулярное выражение\n" + 
+                                "это шаблон для поиска строки в\n" + 
+                                "тексте \n" + 
+                                "В Java исходным представлением этого\n" + 
+                                " шаблона" +
+                                "всегда является строка";
 
         // слова с буквы "с"
         // Pattern pattern = Pattern.compile("\\b[с]{1}\\S+");
@@ -19,8 +23,17 @@ public class RegularExpressions {
         // Pattern pattern = Pattern.compile("^\\S{1,2}\\s+.*$", Pattern.MULTILINE);
 
         // строки из одного слова (и любого количества пробельных символов)
-        Pattern pattern = Pattern.compile("^\\s*\\S*\\s*$", Pattern.MULTILINE);
+        //Pattern pattern = Pattern.compile("^\\s*\\S*\\s*$", Pattern.MULTILINE);
         
+        // строки, которые включают символы и на кириллице, и на латинице
+        //Pattern pattern = Pattern.compile("^.*([a-zA-Z]++.*[а-яА-Я]++|[а-яА-Я]++.*[a-zA-Z]++).*$", Pattern.MULTILINE);
+
+        // строки с прописной буквы
+        //Pattern pattern = Pattern.compile("^[А-ЯA-Z].*$", Pattern.MULTILINE);
+
+        // слова, которые оканчиваются на "ка"
+        Pattern pattern = Pattern.compile("\\b\\S*ка\\b");
+
         Matcher matcher = pattern.matcher(multilineText);
         while (matcher.find()) {
             System.out.println(multilineText.substring(matcher.start(), matcher.end()));
