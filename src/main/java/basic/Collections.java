a7a7e1e36b5664c8e282ff646b6934c8e982680a
CHRONO\chrono <chrono059@gmail.com>
2019-11-06 23:37:05 +0300

    Добавлено заполнение различных коллекций Set

--- src/main/java/basic/Collections.java
+++ src/main/java/basic/Collections.java
@@ -7,10 +7,22 @@ import java.util.*;
  */
 public class Collections {
     public static void main(String[] args) {
-        Set<Integer> collection = new HashSet<Integer>();
+        Random rand = new Random(42);
+
+        List<Set<Integer>> collections = new ArrayList<Set<Integer>>();
+        collections.add(new HashSet<Integer>());
+        collections.add(new TreeSet<Integer>());
+        collections.add(new LinkedHashSet<Integer>());
+
         for (int i = 100; i > 0; i--) {
-            collection.add(i % 10);
+            Integer newItem = rand.nextInt(10);
+            for (Set<Integer> collection : collections) {
+                collection.add(newItem);
+            }
         }
-        System.out.println(collection);
+
+        for (Collection<Integer> collection : collections) {
+            System.out.println(collection.getClass().getSimpleName() + " : " + collection);
+        }
     }    
 }
