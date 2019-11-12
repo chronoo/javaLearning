5df8725b1b7220ff0cc8715d6022b7a075c1cbe3
CHRONO\chrono <chrono059@gmail.com>
2019-11-12 23:49:38 +0300

    Проверена обработка исключений при возвращении значения функцией

--- src/main/java/basic/Exceptions.java
+++ src/main/java/basic/Exceptions.java
@@ -1,36 +1,58 @@
 package basic;
 
 import java.io.Closeable;
-import java.io.IOException;
 
 /**
  * Exceptions
  */
 public class Exceptions {
     public static void main(String[] args) {
+        Integer numb = getNumber();
+        System.out.println(numb);
+    }
+
+    public static Integer getNumber() {
+        try {
+            if (true) {
+                throw new MyExp();
+            }
+            return 42;
+        } catch (NullPointerException e) {
+            System.out.println(e);
+            return 44;
+        // } catch (Exception e) {
+        //     System.out.println(e);
+        //     return 45;
+        }finally{
+            System.out.println("finally");
+            return 43;
+        }
+        // return 44;
+    }
+
+    private static void throwCustomException() {
+        try {
+            try {
+                throw new MyExp("sdf");
+            } catch (MyExp e) {
+                System.out.println(e);
+                throw new Exception();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            throw new MyExp("unchecked");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void useResource() {
         try (MyResource res = new MyResource()) {
             res.justDo();
         } catch (Exception err) {
             System.out.println(err);
         } finally {
             System.out.println("finally-block");
-        }
-    }
-
-    public static class MyResource implements Closeable {
-
-        public MyResource() {
-        }
-        
-        public void justDo() {
-            System.out.println("just do");
-            System.out.println("do something with resource");
-            throw new RuntimeException("main");
-        }
-        @Override
-        public void close() {
-            System.out.println("close");
-            throw new RuntimeException("resource");
         }
     }
 
@@ -59,3 +81,29 @@ public class Exceptions {
         System.out.println("main finish");
     }
 }
+
+class MyExp extends RuntimeException {
+    public MyExp(){
+        super();
+    }
+    public MyExp(String msg) {
+        super(msg);
+    }
+}
+
+class MyResource implements Closeable {
+    public MyResource() {
+    }
+    
+    public void justDo() {
+        System.out.println("just do");
+        System.out.println("do something with resource");
+        throw new RuntimeException("main");
+    }
+    @Override
+    public void close() {
+        System.out.println("close");
+        throw new RuntimeException("resource");
+    }
+}
+
