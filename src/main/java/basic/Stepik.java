e1c3e6dbe0d37ec1313e12ffa3c358cc84ae1173
CHRONO\chrono <chrono059@gmail.com>
2019-11-17 23:51:03 +0300

    Добавлено ещё несколько задач

--- src/main/java/basic/Stepik.java
+++ src/main/java/basic/Stepik.java
@@ -1,33 +1,77 @@
 package basic;
 
+import java.math.BigInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Boolean
  */
 public class Stepik {
     public static void main(String[] args) {
-        for (int i = -4; i < 100; i++) {
-            System.out.println(i + ":" + isPowerOfTwo(i));
-        }
+        System.out.println(factorial(1));
     }
 
-      // пункт 2.3, 10 шаг
-      public static boolean isPowerOfTwo(int value) {
+     // пункт 2.4, 8 шаг
+     public static BigInteger factorial(int value) {
+        BigInteger result = BigInteger.valueOf(value);
+        for (int i = value - 1; i > 1; i--) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
+    }
+    // пункт 2.3, 10 шаг
+    public static boolean isPalindrome1(String text) {
+        String result = text.replaceAll("[^a-zA-Z0-9]", "");
+        StringBuilder sb = new StringBuilder(result);
+        String reverse = sb.reverse().toString();
+        return result.equalsIgnoreCase(reverse);
+    }
+
+    // пункт 2.3, 10 шаг
+    public static boolean isPalindrome(String text) {
+        String upperCase = text.toUpperCase();
+        Pattern pattern = Pattern.compile("[A-Z0-9]");
+        Matcher matcher = pattern.matcher(upperCase);
+        StringBuilder sb = new StringBuilder();
+        while (matcher.find()) {
+            sb.append(upperCase.substring(matcher.start(), matcher.end()));
+        }
+        String origin = sb.toString();
+        String reverse = sb.reverse().toString();
+        return origin.equals(reverse);
+    }
+
+    // пункт 2.3, 8 шаг
+    private static void stringConcat() {
+        String string1 = "A" + ('\t' + '\u0003');
+        String string2 = "A" + 12;
+        String string3 = 'A' + '1' + "2";
+        String string4 = 'A' + "12";
+        System.out.println(string1);
+        System.out.println(string2);
+        System.out.println(string3);
+        System.out.println(string4);
+    }
+
+    // пункт 2.2, 10 шаг
+    public static boolean isPowerOfTwo(int value) {
         return java.lang.Integer.bitCount(Math.abs(value)) == 1;
     }
 
-     // пункт 2.3, 9 шаг
-     public static void typeTransfer() {
+    // пункт 2.2, 9 шаг
+    public static void typeTransfer() {
         // int a = 4;
         // String b = "a";
-        // a = b;          // String->int
-        // boolean c = a;  // int->boolean
-        // long d = 1;   
-        // float e = d;    // long->float +
-        // d = a;          // int->long +
-        // d = e;          // float->long
+        // a = b; // String->int
+        // boolean c = a; // int->boolean
+        // long d = 1;
+        // float e = d; // long->float +
+        // d = a; // int->long +
+        // d = e; // float->long
         // byte f = 4;
-        // char g = f;     // byte->char
-        // a = g;          // chat->int +
+        // char g = f; // byte->char
+        // a = g; // chat->int +
         // Character h = g;// char->Character +
     }
 
@@ -35,9 +79,9 @@ public class Stepik {
     public static char charExpression(int a) {
         return (char) ('\\' + a);
     }
-    
-     // пункт 2.1, 13 шаг
-     public static int flipBit(int value, int bitIndex) {
+
+    // пункт 2.1, 13 шаг
+    public static int flipBit(int value, int bitIndex) {
         return value ^= (1 << bitIndex - 1);
     }
 
@@ -46,9 +90,9 @@ public class Stepik {
         return Math.abs((a + b) - c) < 0.0001;
     }
 
-     // пункт 2.1, 7 шаг
-     public static int leapYearCount(int year) {
-        return year/4 - year/100 + year/400;
+    // пункт 2.1, 7 шаг
+    public static int leapYearCount(int year) {
+        return year / 4 - year / 100 + year / 400;
     }
 
     // пункт 2.1, 5 шаг
@@ -67,7 +111,7 @@ public class Stepik {
 
         return trueCount == 2;
     }
-    
+
     public static void opreatorTest() {
         boolean[] a = { false, true };
 
