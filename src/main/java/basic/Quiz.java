030b6ce3bd8b8ccdd7b687f346bb13f0302da08a
CHRONO\chrono <chrono059@gmail.com>
2019-11-20 07:02:16 +0300

    Добавлено вводное задание

---
+++ src/main/java/basic/Quiz.java
@@ -0,0 +1,16 @@
+package basic;
+import java.security.MessageDigest;
+
+/**
+ * Quiz
+ */
+public class Quiz {
+
+    public static void main(String[] args) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] digest = md.digest("abracadabra".getBytes("UTF-8"));
+        for (byte b : digest) {
+            System.out.printf("%02x", b);
+        }
+    }
+}
